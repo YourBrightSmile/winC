77c662994beb02079d534541371c9374dee19a9d
YourBrightSmile <914552504@qq.com>
2024-03-25 18:12:27 +0800

    wininfo

--- lib/winInfoLib.py
+++ lib/winInfoLib.py
@@ -1,16 +1,20 @@
 #!/bin/python3
+import os
+
 import pynvml
 import wmi
 
 winInfoDict = ['']
 w = wmi.WMI()
 
-def getOperatingSystem():
+def getOsInfo():
     result = {}
     # systeminfo
     for system in w.Win32_OperatingSystem():
         tmp = {}
-        tmp['systemname'] = system.Caption
+        usernameFull = os.environ['userdomain'] + '\\' + os.getlogin()+'@'+os.environ['COMPUTERNAME']
+        tmp['osname'] = system.Caption
+        tmp['usernameFull'] = usernameFull
         result['OperatingSystem'] = tmp
 
     w.Win32_Processor()
@@ -25,3 +29,15 @@ def getOperatingSystem():
     # w.Win32_PerfFormattedData_Counters_ThermalZoneInformation()[0].Temperature-273
     w.Win32_PerfFormattedData_Counters_ThermalZoneInformation()[0].Temperature
     # pynvml.nvmlDeviceGetTemperature(handle, 0)
+'''
+nop@SZMGINB1409A
+----------------
+Uptime: 14 days, 8 hours, 12 mins
+IP: 1.1.1.1(WLAN)/2.2.2.2(ETH)/3.3.3.3(FIBER)
+MAC: AA:BB:CC:DD:EE:FF/
+CPU: Intel i5-5300U (4) @ 2.294GHz
+GPU: Intel i5-5300U (4) @ 2.294GHz
+Memory: 97MiB / 12543MiB
+Disk: DISKNAME/DISKNAME/DISKNAME/DISKNAME
+OS: Windows 10 x86_64
+'''
